13ce048c80818101a3df2c1311da5f59480f8f52
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2025-02-28 18:24:49 +0200

    Added user authentication functionality

--- api/models.nim
+++ api/models.nim
@@ -1,12 +1,19 @@
 import norm/[types, model]
+import std/options
 
 type
   Player* = ref object of Model
     code*: PaddedStringOfCap[8]
+    authToken*: Option[PaddedStringOfCap[12]]
     money*: int32
 
-func newPlayer*(code: string = "", money: int32 = 0): Player =
+func newPlayer*(
+  code: string = "",
+  money: int32 = 0,
+  authToken: Option[PaddedStringOfCap[12]] = none PaddedStringOfCap[12]
+  ): Player =
   Player(
     code: newPaddedStringOfCap[8](code),
-    money: money
+    money: money,
+    authToken: authToken
   )
--- api/routes.nim
+++ api/routes.nim
@@ -1,5 +1,5 @@
 import "mummy_base.nim"
-import std/[sysrand, base64, strutils]
+import std/[sysrand, base64, strutils, tables, options]
 import "security.nim"
 import "valkey.nim" as _
 import "psql_base.nim"
@@ -7,11 +7,22 @@ import "psql_base.nim"
 let valkey = valkeyPool
 
 get "/init":
-  headers["Content-Type"] = "text/plain"
+
+  let reqCookies = parseCookies request.headers["Cookie"]
+  let hasAuthCookie = reqCookies.hasKey("a")
+  let authCookie = reqCookies["a"]
+  var isLoggedIn = false
+
+  psql:
+    isLoggedIn = db.exists(Player, "authToken = $1", authCookie)
+
   let
     counterData = valkey.command("GET", "valkeyTest").to(string)
     motdData = valkey.command("GET", "currentMotd").to(string)
-    content = $counterData & ":" & motdData
+    authData = if isLoggedIn: "1" else: "0"
+    content = $counterData & ":" & motdData & ":" & authData
+
+  headers["Content-Type"] = "text/plain"
   resp 200, content
 
 get "/challenge":
@@ -34,7 +45,7 @@ post "/register":
     sentSignature = body[1]
     sentSecretNumber = body[2].parseInt
   if submitPowResponse(sentSalt, sentSignature, sentSecretNumber):
-    let newCode: string = base64.encode urandom(6) # specifying base64 because 'encode' is quite vague
+    let newCode: string = secureRandomBase64(6)
     var playerQuery = newPlayer(newCode)
     psql:
       db.insert(playerQuery)
@@ -61,25 +72,24 @@ post "/login":
     sentSalt = body[1]
     sentSignature = body[2]
     sentSecretNumber = body[3].parseInt
-  if submitPowResponse(sentSalt, sentSignature, sentSecretNumber):
-    var codeValid = false
-    psql:
-      codeValid = db.exists(Player, "code = $1", sentCode)
-    if codeValid:
-      headers["Content-Type"] = "text/plain"
-      headers["Set-Cookie"] = makeCookie(
-        "a", secureRandomHexadecimal(8),
-        expires=daysForward(7),
-        path="/",
-        secure=true,
-        httpOnly=true,
-        sameSite=Strict
-      )
-      resp 200, "auth token goes here"
-    else:
-      resp 404
-  else:
-    resp 401
+  if not submitPowResponse(sentSalt, sentSignature, sentSecretNumber): resp 401
+  psql:
+    let codeValid = db.exists(Player, "code = $1", sentCode)
+    if not codeValid: resp 404
+    let authCookie = secureRandomBase64(9)
+    var playerQuery = newPlayer()
+    db.select(playerQuery, "code = $1", sentCode)
+    playerQuery.authToken = some newPaddedStringOfCap[12](authCookie)
+    db.update(playerQuery)
+    headers["Set-Cookie"] = makeCookie(
+      "a", authCookie,
+      expires=daysForward(7),
+      path="/",
+      secure=true,
+      httpOnly=true,
+      sameSite=Strict
+    )
+  resp 200
 
 post "/delete":
   let body = request.body.split(":")
@@ -114,3 +124,21 @@ post "/delete":
         resp 404
   else:
     resp 401
+
+post "/logout":
+  let reqCookies = parseCookies request.headers["Cookie"]
+  if not reqCookies.hasKey("a"): resp 401
+  let authCookie = reqCookies["a"]
+  psql:
+    # This check might be a bit useless, will keep it for now incase of debugging
+    if not db.exists(Player, "authToken = $1", authCookie): resp 404
+    db.exec(sql "UPDATE \"Player\" SET authToken = NULL WHERE authToken = $1", authCookie)
+  headers["Set-Cookie"] = makeCookie(
+    "a", "",
+    expires=daysForward(-1),
+    path="/",
+    secure=true,
+    httpOnly=true,
+    sameSite=Strict
+  )
+  resp 200
